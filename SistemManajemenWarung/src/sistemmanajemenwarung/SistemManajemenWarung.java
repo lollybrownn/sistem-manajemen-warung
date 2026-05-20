b057bce1e28bc4b69f842c071dbf9fe1c5efc1af
Albert <albertusbagus.dya@gmail.com>
2026-05-20 20:54:07 +0700

    Add SistemManajemenWarung NetBeans project
    Import NetBeans project skeleton for "SistemManajemenWarung". Adds Ant build files (build.xml, nbproject/* including generated build-impl.xml), manifest and build metadata (built-jar.properties), compiled resources under build/classes, and source UI assets (src/FXML/Login.fxml, src/CSS/Login.css) plus main application class src/sistemmanajemenwarung/SistemManajemenWarung.java. Initializes project and login UI layout; build-impl.xml is generated by NetBeans and should be customized via build.xml when needed.

---
+++ SistemManajemenWarung/src/sistemmanajemenwarung/SistemManajemenWarung.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemmanajemenwarung;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SistemManajemenWarung extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/Login.fxml"));
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
